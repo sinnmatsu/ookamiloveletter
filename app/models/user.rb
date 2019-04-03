7b5f664604846768189a68e2af330bac20fada20
takuma.shinmatsu <takuma@icloud.com>
2019-04-03 03:49:32 +0000

    common

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: { maximum: 50 }
+  before_save { self.email.downcase! }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+end
